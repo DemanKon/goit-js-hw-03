5926af7f0188fa173732c7aa5a58839efbb9b374
DemanKon <deman.vk@gmail.com>
2024-04-01 23:50:01 +0300

    goit-js-hw-03 done

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+// Напиши функцію під назвою makeArray, яка приймає три параметри:
+// firstArray(масив),
+// secondArray(масив) і
+// maxLength(число).
+// Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+function makeArray(firstArray, secondArray, maxLength) {
+    //console.log("🚀 ~ makeArray ~ maxLength:", maxLength)
+    
+    const newArray = firstArray.concat(secondArray);
+    //console.log("🚀 ~ makeArray ~ newArray:", newArray)
+    
+if (newArray.length > maxLength) {
+    const sliceArray = newArray.slice(0, maxLength);
+    //console.log("🚀 ~ makeArray ~ sliceArray:", sliceArray)
+    
+}
+        
+
+
+}
+
+
+makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3); // ["Mango", "Poly", "Ajax"]
+makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4); // ["Mango", "Poly", "Houston", "Ajax"]
+makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3); // ["Mango", "Ajax", "Chelsea"]
+makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2); // ["Earth", "Jupiter"]
+makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0); // []
